4f1fb34b87379090215cd7075c99fa1c16cfdb97
hcg0127 <kej431003@gmail.com>
2025-05-07 20:29:23 +0900

    [fix] MemberMissionRepository가 MemberMissionRepositoryCustom도 상속하도록 수정

--- src/main/java/umc/spring/repository/MemberMissionRepository/MemberMissionRepository.java
+++ src/main/java/umc/spring/repository/MemberMissionRepository/MemberMissionRepository.java
@@ -3,5 +3,5 @@ package umc.spring.repository.MemberMissionRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import umc.spring.domain.mapping.MemberMission;
 
-public interface MemberMissionRepository extends JpaRepository<MemberMission, Long> {
+public interface MemberMissionRepository extends JpaRepository<MemberMission, Long>, MemberMissionRepositoryCustom {
 }
