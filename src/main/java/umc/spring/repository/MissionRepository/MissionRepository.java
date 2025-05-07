29d2208a306e3debba6dc5bb49b84206be86b31b
hcg0127 <kej431003@gmail.com>
2025-05-07 22:01:42 +0900

    [feat] 6주차 미션-2: 홈 화면 쿼리 service 코드 작성

--- src/main/java/umc/spring/repository/MissionRepository/MissionRepository.java
+++ src/main/java/umc/spring/repository/MissionRepository/MissionRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import umc.spring.domain.Mission;
 import umc.spring.repository.MemberMissionRepository.MemberMissionRepositoryCustom;
 
-public interface MissionRepository extends JpaRepository<Mission, Long>, MemberMissionRepositoryCustom {
+public interface MissionRepository extends JpaRepository<Mission, Long>, MissionRepositoryCustom {
 }
